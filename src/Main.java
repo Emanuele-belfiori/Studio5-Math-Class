145c81847abdd0bf43a99a2d65111dd1c230d048
Emanuele-belfiori <121684864+Emanuele-belfiori@users.noreply.github.com>
2023-01-21 11:06:19 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Math Class: ");
+
+        /*
+         *  Math class:
+         *
+         *  - alcuni metodi: abs, min, max, pow, sqrt
+         *
+         *  - attributi: PI
+         */
+
+        System.out.println(Math.abs(-55)); // Numero assoluto toglierà il segno
+
+        System.out.println(Math.max(55,21)); // Troverà il massimo valore
+
+        System.out.println(Math.min(55,21)); // Troverà il minimo valore
+
+        System.out.println(Math.pow(5, 3)); // Potenza= calcolerà 5 alla 3°
+
+        System.out.println(Math.sqrt(25)); // Calcola la radice quadrata di 25
+
+        System.out.println(Math.PI); // Calcola il P Grco (¶)
+    }
+}
